67cf3cd7b94efd599dbdcea88edfe28ac1757574
sbnoman01 <sbnoman27@gmal.com>
2023-02-18 02:14:11 +0600

    DB update

--- job_list.sql
+++ job_list.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 09, 2023 at 05:12 AM
--- Server version: 8.0.30
--- PHP Version: 7.4.32
+-- Generation Time: Feb 17, 2023 at 08:13 PM
+-- Server version: 5.7.33
+-- PHP Version: 7.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,19 +25,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
+(1, 'Web Developer'),
+(2, 'Web Designer');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `job_list`
 --
 
 CREATE TABLE `job_list` (
-  `job_id` int NOT NULL,
-  `cat_id` varchar(255) NOT NULL,
-  `company_name` varchar(255) NOT NULL,
-  `job_title` varchar(255) NOT NULL,
-  `job_desc` varchar(255) NOT NULL,
-  `job_salery` int NOT NULL,
-  `job_location` varchar(255) NOT NULL,
-  `user_contact` varchar(255) NOT NULL,
-  `email_contact` varchar(255) NOT NULL
+  `job_id` int(11) NOT NULL,
+  `cat_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `job_title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `job_desc` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `job_salery` int(11) NOT NULL,
+  `job_location` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_contact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_contact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -44,13 +64,20 @@ CREATE TABLE `job_list` (
 --
 
 INSERT INTO `job_list` (`job_id`, `cat_id`, `company_name`, `job_title`, `job_desc`, `job_salery`, `job_location`, `user_contact`, `email_contact`) VALUES
-(1, '1', 'Noman', 'Wordpress Dev', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aperiam consectetur repudiandae nostrum, ad atque quisquam natus distinctio quos nesciunt dolores ipsa dolor esse omnis iusto, laudantium blanditiis, vitae nam?\r\n', 50, 'Dhaka', '0155889966', 'sbnoman@gmail.com'),
-(2, '2', 'Abdullah', 'Wordpress Dev', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aperiam consectetur repudiandae nostrum, ad atque quisquam natus distinctio quos nesciunt dolores ipsa dolor esse omnis iusto, laudantium blanditiis, vitae nam?\r\n', 30, 'Dhaka', '0155889966', 'ab@gmail.com'),
-(3, '1', 'Jamal', 'Wordpress Dev', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aperiam consectetur repudiandae nostrum, ad atque quisquam natus distinctio quos nesciunt dolores ipsa dolor esse omnis iusto, laudantium blanditiis, vitae nam?\r\n', 50, 'Dhaka', '0155889966', 'jamal@gmail.com');
+(1, '1', 'WP noman', 'Noman wp', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aperiam consectetur repudiandae nostrum, ad atque quisquam natus distinctio quos nesciunt dolores ipsa dolor esse omnis iusto, laudantium blanditiis, vitae nam?\r\n', 50, 'Dhaka', '0155889966', 'sbnoman@gmail.com'),
+(2, '1', 'Abdullah An Noman', 'Wordpress Dev', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aperiam consectetur repudiandae nostrum, ad atque quisquam natus distinctio quos nesciunt dolores ipsa dolor esse omnis iusto, laudantium blanditiis, vitae nam?\r\n', 50, 'Dhaka', '0155889966', 'sbnoman@gmail.com'),
+(3, '1', 'Jamal', 'Wordpress Dev', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto aperiam consectetur repudiandae nostrum, ad atque quisquam natus distinctio quos nesciunt dolores ipsa dolor esse omnis iusto, laudantium blanditiis, vitae nam?\r\n', 50, 'Dhaka', '0155889966', 'jamal@gmail.com'),
+(4, '2', 'Unlocklive IT', 'Wordpress Developer', 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without ', 2500, 'Dhaka', '018789', 's@gmail.com');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
 
 --
 -- Indexes for table `job_list`
@@ -63,10 +90,16 @@ ALTER TABLE `job_list`
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `job_list`
 --
 ALTER TABLE `job_list`
-  MODIFY `job_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
